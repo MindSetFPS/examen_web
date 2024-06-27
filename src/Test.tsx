4d3cb4b3682b3c004d03644ae6cfa04bb7630b90
Daniel Escobar <daanielescobar@gmail.com>
2024-06-27 12:10:05 -0600

    project planning

---
+++ src/Test.tsx
@@ -0,0 +1,21 @@
+
+import questionList from './questionType'
+
+export default function Test(){
+    // funcion para seleccionar las preguntas
+
+    // variable use state para saber en que pregunta estoy 
+
+    // funcion en boton para pasar a la siguiente pregunta
+
+    // si estoy en la ultima pregunta 
+        // mostrar los aciertos
+    
+    return (
+        <>
+            tu papa
+
+            <button />
+        </>
+    )
+}
