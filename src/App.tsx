410f3aae27f216b8cf05cf8469bee8665d0754d7
Daniel Escobar <daanielescobar@gmail.com>
2024-06-29 16:05:34 -0600

    que pasara?

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,24 @@
 import { useState } from "react";
-import { ToTest } from "./components/ToTest";
-import { ListOfQuestions } from "./types";
-
-const initialQuestions: ListOfQuestions = [
-  {
-    id: "1",
-    question: "¿Cuál es la capital de Francia?",
-    options: ["Madrid", "París", "Roma", "Berlín"],
-    answer: "París",
-    completed: false,
-  },
-  {
-    id: "2",
-    question: "¿Cuál es el río más largo del mundo?",
-    options: ["Amazonas", "Nilo", "Yangtsé", "Misisipi"],
-    answer: "Nilo",
-    completed: false,
-  },
-];
+import { Test } from "./components/Test";
+import { ResultScreen} from "./components/ResultScreen";
 
 const App = (): JSX.Element => {
-  const [questions, setQuestions] = useState<ListOfQuestions>(initialQuestions);
-
-  const handleRemove = (id: string) => {
-    setQuestions((prevQuestions) => prevQuestions.filter((q) => q.id !== id));
-  };
-
-  const handleSelectOption = (id: string, selectedOption: string) => {
-    setQuestions((prevQuestions) =>
-      prevQuestions.map((q) => (q.id === id ? { ...q, selectedOption } : q))
-    );
-  };
+  const [testFinished, setTestFinished] = useState<Boolean>(false);
 
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
         <h1>Examen en Línea</h1>
-        {questions.map((q) => (
-          <ToTest
-            key={q.id}
-            id={q.id}
-            question={q.question}
-            options={q.options}
-            answer={q.answer}
-            completed={q.completed}
-            onRemoveAns={handleRemove}
-            onSelectionOption={handleSelectOption}
-          />
-        ))}
-      </header>
+
+        {
+          testFinished ? 
+            <ResultScreen/> 
+            : 
+            <Test/>
+          }
+
+
+      </div>
     </div>
   );
 };
