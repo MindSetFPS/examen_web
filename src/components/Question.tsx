f3f9b62b76fda8b0355648ce96c6c683f7e8b386
Daniel Escobar <daanielescobar@gmail.com>
2024-07-02 00:00:10 -0600

    remove things, prepare events

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -2,48 +2,40 @@ import { useState } from "react";
 import { type Questions } from "../types";
 
 interface Props extends Questions {
-  onRemoveAns: (id: string) => void;
-  onSelectionOption: (id: string, selectedOption: string) => void;
+  onChildEvent: (event: React.MouseEvent<HTMLInputElement>) => void;
 }
-export const Question: React.FC<Questions> = ({
+export const Question: React.FC<Props> = ({
   id,
   text,
   answerOptions,
   correctAnswer,
   completed,
-  // onRemoveAns,
-  // onSelectionOption,
+  onChildEvent,
 }) => {
-  const [selectedOption, setSelectedOption] = useState<string | null>(null);
-
-  const handleOptionChange = (option: string) => {
-    setSelectedOption(option);
-    // onSelectionOption(id, option);
-  };
+  function setIsCorrectAnswer() {
+    onChildEvent()
+  }
   return (
     <div className="view">
-      <label>{text}</label>
-      <div>
-        {answerOptions.map((answerOption) => (
-          <div key={id}>
-            <input
-              type="radio"
-              id={`${id}`}
-              name={`question-${id}`}
-              value={answerOption}
-              // checked={selectedOption === option}
-              // onChange={() => handleOptionChange(option)}
-            />
-            <label htmlFor={`${id}-${answerOption}`}>{answerOption}</label>
-          </div>
-        ))}
+      <h2 className="font-medium text-3xl">{text}</h2>
+      <div >
+        <form>
+          {
+            answerOptions.map((option) => (
+              <div className={`bg-white ${option == correctAnswer ? 'bg-green-100' : 'bg-red-100'}`}>
+                <span >
+                  {'🍿'}
+                </span>
+                <input
+                  type="radio"
+                  value={option}
+                  onClick={setIsCorrectAnswer} />
+                {option}
+              </div>
+            ))
+          }
+        </form>
       </div>
-      {/* <button
-        className="destroy"
-        onClick={() => {
-          onRemoveAns(id);
-        }}
-      > Enviar </button> */}
-    </div>
-  );
-};
+
+    </div>)
+}
