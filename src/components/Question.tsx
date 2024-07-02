8d325fac92d23b68bf5c870de85afff78fe58442
Daniel Escobar <daanielescobar@gmail.com>
2024-07-02 02:09:37 -0600

    single radio buton, hide or show next button, finish text

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -2,7 +2,8 @@ import { useState } from "react";
 import { type Questions } from "../types";
 
 interface Props extends Questions {
-  onChildEvent: (event: React.MouseEvent<HTMLInputElement>) => void;
+  onChildEvent: () => void;
+  onShowNextButton: () => void;
 }
 export const Question: React.FC<Props> = ({
   id,
@@ -11,10 +12,17 @@ export const Question: React.FC<Props> = ({
   correctAnswer,
   completed,
   onChildEvent,
+  onShowNextButton
 }) => {
-  function setIsCorrectAnswer() {
+
+  const [selectedOption, setSelectedOption] = useState<string | null>(null)
+
+  const handleOptionClick = (option: string) => {
+    setSelectedOption(option);
     onChildEvent()
+    onShowNextButton()
   }
+
   return (
     <div className="view">
       <h2 className="font-medium text-3xl">{text}</h2>
@@ -29,13 +37,13 @@ export const Question: React.FC<Props> = ({
                 <input
                   type="radio"
                   value={option}
-                  onClick={setIsCorrectAnswer} />
+                  checked={selectedOption == option}
+                  onChange={() => handleOptionClick(option)} />
                 {option}
               </div>
             ))
           }
         </form>
       </div>
-
     </div>)
 }
--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,12 +1,6 @@
 import { type ListOfQuestions } from "../types";
-import { Answer } from "./Answers";
 import { useState } from "react";
 import { Question } from "./Question";
-
-interface Props {
-  totest: ListOfQuestions;
-  onRemoveAns: (id: string) => void;
-}
 
 const questionsList: ListOfQuestions = [
   {
@@ -25,77 +19,64 @@ const questionsList: ListOfQuestions = [
   },
 ];
 
-export const Test: React.FC = () => {
-  
-  const [questions, setQuestions] = useState<ListOfQuestions>(questionsList);
+interface Props {
+  onTestFinished: (data: string) => void;
+}
 
+export const Test: React.FC<Props> = ({ onTestFinished }) => {
+  // const [questions, setQuestions] = useState<ListOfQuestions>(questionsList);
   const [currentQuestion, setCurrentQuestion] = useState(0);
 
-/*   const handleRemove = (id: string) => {
-    setQuestions((prevQuestions) => prevQuestions.filter((q) => q.id !== id));
-  };
+  const [correctAnswerCounter, setCorrectAnswerCounter] = useState<number>(0);
+  const [showNextButton, setShowNextButton] = useState<boolean>(false)
 
-  const handleSelectOption = (id: string, selectedOption: string) => {
-    setQuestions((prevQuestions) =>
-      prevQuestions.map((q) => (q.id === id ? { ...q, selectedOption } : q))
-    );
-  };
-  */ 
-  function nextQuestion(){
-    
-    if(questionsList.length != currentQuestion + 0){
-      console.log('jamas van a ser iguales')
+  function nextQuestion() {
+    if (questionsList.length != currentQuestion + 1) {
       setCurrentQuestion(currentQuestion + 1);
     }
 
-    handleTestFinished()
+    if (questionsList.length == currentQuestion + 1) {
+      handleTestFinished()
+    }
   }
-  
-  function handleTestFinished(){
-    //
+
+  function handleTestFinished() {
+    console.log('test finished')
+    onTestFinished(String(correctAnswerCounter))
   }
 
   return (
     <ul className="test-list rounded-xl shadow-lg p-2">
+      <Question
+        id={questionsList[currentQuestion].id}
+        text={questionsList[currentQuestion].text}
+        answerOptions={questionsList[currentQuestion].answerOptions}
+        correctAnswer={questionsList[currentQuestion].correctAnswer}
+        completed={questionsList[currentQuestion].completed}
+        onChildEvent={() => setCorrectAnswerCounter(correctAnswerCounter + 1)}
+        onShowNextButton={() => setShowNextButton(true)}
+      // onRemoveAns={handleRemove}
+      // onSelectionOption={handleSelectOption}
+      />
 
-     
-        <Question
-          id={questionsList[currentQuestion].id}
-          text={questionsList[currentQuestion].text}
-          answerOptions={questionsList[currentQuestion].answerOptions}
-          correctAnswer={questionsList[currentQuestion].correctAnswer}
-          completed={questionsList[currentQuestion].completed}
-          onChildEvent={() => console.log('ola')}
-          // onRemoveAns={handleRemove}
-          // onSelectionOption={handleSelectOption}
-        />
+      {
+        showNextButton ?
+          <button
+            className="destroy"
+            onClick={() => nextQuestion()}
+          >
+            {questionsList.length == currentQuestion + 1 ? 'amonos' : 'siguiente'}
+          </button>
+          :
+          ''
+        }
 
-        <button
-          className="destroy"
-          onClick={() => nextQuestion() }
-        > { questionsList.length == currentQuestion + 1 ? 'amonos': 'siguiente'} </button>
-
-
-{/*       {totest.map((item) => (
-        <li key={item.id} className={`${item.completed ? "completed" : ""}`}>
-          <Answer
-            key={item.id}
-            id={item.id}
-            question={item.question}
-            answer={item.answer}
-            completed={item.completed}
-          />
-        </li>
-      ))} */}
     </ul>
   );
 };
 
 // funcion para seleccionar las preguntas
-
 // variable use state para saber en que pregunta estoy
-
 // funcion en boton para pasar a la siguiente pregunta
-
 // si estoy en la ultima pregunta
 // mostrar los aciertos
